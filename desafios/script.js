589eab9ce9f78e44bcf14c782cd0c2e896710538
Gabriel Felipe <168776671+Gabrielfllazaro@users.noreply.github.com>
2025-05-23 22:20:01 -0300

    Add files via upload

---
+++ desafios/script.js
@@ -0,0 +1,12 @@
+// Crie uma função chamada dobro que receba um número como parâmetro e retorne seu dobro.
+
+// function dobro(n) {
+//   // seu código aqui
+// }
+function dobro(n) {
+  return n * 2;
+}
+
+
+let numero = parseFloat(prompt("Digite um número:"));
+console.log( dobro(numero));
